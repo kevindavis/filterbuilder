24bc80ce4c5cf1a5aa6c190f9d8a15e3c9b76598
Kevin Davis <kgdavis@gmail.com>
2013-05-27 18:31:24 -0700

    Redone using backbone.js

--- TODO.markdown
+++ TODO.markdown
@@ -1,12 +1,10 @@
 ## UX tweaks 
 * shouldn't be able to remove the last clause (hide with CSS :first?)
-* style for same sized dropdowns
-* nicer looking remove
+* bootstrap styling
 
 ## Bigger issues
-* should actually spit out a JSON representation
-* rework data structure so don't have to loop through to find elements (comparator as key in the hash?)
 * any/all switch? 
-* allow user to refer to fields?
-* package as a JQuery plugin
-* mechanism for attaching a function to arguments so you can do things like make it an autocomplete, date picker etc
+* package for external consumption somehow
+* validation for numbers
+* date picker
+* let developer attach picking functions to the input boxes
--- filterbuilder.js
+++ filterbuilder.js
@@ -1,216 +1,178 @@
-var template;
+$(function(){
 
-// Conditions vary by type
-// number: is, is not, greater than, less than, between
-// date: before, after, between
-// string: contains, doesn't contain, is, is not
-var comparators = {
-	"Number" : 
-		[{"Comparator": "is",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "is not",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "greater than",
-			"ArgumentCount" : 1,
-				"ArgumentDefaults" : [] },
-		 {"Comparator" : "less than",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "between",
-			"ArgumentCount" : 2,
-			"ArgumentDefaults" : [] }
-		 ],
-		
-	"Date" : 
-		[{"Comparator": "is",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "is not",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "before",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "after",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-			{"Comparator" : "between",
-			"ArgumentCount" : 2,
-			"ArgumentDefaults" : [] }
-		],
-		
-	"String" : 
-		[{"Comparator": "is",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "is not",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "contains",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "does not contain",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "begins with",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] },
-		 {"Comparator" : "ends with",
-			"ArgumentCount" : 1,
-			"ArgumentDefaults" : [] }
-		]
-};
+	var Field = Backbone.Model.extend({
+		VALID_FIELD_TYPES: ['string', 'date', 'number'],
 
-function setup(fields, existing_clauses){
+		defaults: function(){
+			return {
+				fieldType: 'string'
+			};
+		},
 
-	// create a template that looks like:
-	// <div class="clause">
-	// 	<select class="fields">
-	// 		<option data-fieldtype="FieldType1">FieldName1</option>
-	// 		<option data-fieldtype="FieldType2">FieldName2</option>
-	// 	</select>
-	//  <select class="comparators"></select>
-	// 	<span class="conditions"></span>
-	// 	<button class="remove-clause">remove</button>
-	// </div>
-	template = $("<div/>", { class: "clause" } );
-	var fields_dropdown = $("<select/>", { class: "fields"});
-	for(var i in fields)
-	{
-		$("<option/>")
-			.attr("data-fieldtype", fields[i].FieldType)
-			.text(fields[i].FieldName)
-				.appendTo(fields_dropdown);
-	}
-	fields_dropdown.change(function(){
-		adjustField($(this).parent(), $(this).find('option:selected').attr('data-fieldtype'));
-	});
-	fields_dropdown.appendTo(template);
-	var comparators_dropdown = $("<select/>", { class: "comparators"})
-	comparators_dropdown.change(function(){
-			adjustComparator($(this).parent(), $(this).val());
-		})
-		comparators_dropdown.appendTo(template);
-	$("<span/>", { class: "conditions"})
-		.appendTo(template);
-	$("<button/>")
-		.text("remove")
-		.click(function(){
-			$(this).parent().remove();
-		})
-		.appendTo(template);
-
-	adjustField(template, fields[0].FieldType);
-	
-	if(existing_clauses.size == 0){
-		template.clone(true).appendTo('#clauses');
-	}
-				
-	// hydrate the existing clauses
-	for(var i in existing_clauses)
-	{
-		addClause(existing_clauses[i]);
-	}
-}
-
-function addClause(clause){
-	
-	var new_clause = template.clone(true);
-	
-	if(clause){
-		
-		// Expects a clause that looks like:
-		// {
-		// 	"FieldName" : "Title",
-		// 	"Condition" : {
-		// 		"Comparator" : "is",
-		// 		"Arguments" : ["1984"]
-		// 	}
-		// },
-
-		// set the field
-		new_clause.find('.fields').val(clause.FieldName);
-		adjustField(new_clause, $('.fields option:selected', new_clause).attr('data-fieldtype'));
-		
-		// set the comparator
-		new_clause.find('.comparators').val(clause.Condition.Comparator);
-		adjustComparator(new_clause, clause.Condition.Comparator);
-		
-		// fill in the arguments
-		var inputs = new_clause.find('.conditions input');
-		if(clause.Condition.Arguments.length == 1){
-			inputs.val(clause.Condition.Arguments[0]);
-		} else {
-			for (var i in clause.Condition.Arguments){
-				inputs[i].value = clause.Condition.Arguments[i];
-			}			
-			
+		validation: {
+			fieldType: { oneOf: this.VALID_FIELD_TYPES }
 		}
-	}
-	
-	new_clause.appendTo('#clauses');
-}
-		
-// change the condition UI to fit the selected field
-// expects: clause: a jquery object representing a clause div
-// 					fieldtype: a type of field.. surprise!
-function adjustField(clause, fieldtype){
-	// clear the options from the .comparators select
-	var dropdown = $(clause).find(".comparators");
-	dropdown.find("option").remove();
+	});
 
-	// fill in the new options
-	for (var i in comparators[fieldtype])
-	{
-		$("<option/>")
-			.text(comparators[fieldtype][i].Comparator)
-			.appendTo(dropdown);
-	}
-	adjustComparator(clause,comparators[fieldtype][0]);
-}
+	var Clause = Backbone.Model.extend({
+		COMPARATORS: {
+			string: ['is', 'is not', 'contains'],
+			date: 	['is', 'is not', 'between', 'before', 'after'],
+			number: ['is', 'is not', 'between', 'less than', 'greater than']
+		},
 
-// change the parameters UI to fit the condition specified
-// expects: clause: a jquery object representint a clause div
-// 					comparator: a comparator (string OR object) eg. "is", "greater than" etc
-function adjustComparator(clause, comparator){
-	// clear the previous conditions
-	var conditions = clause.find(".conditions");
-	conditions.find("*").remove();
-	
-	// form the comparator object out of a string
-	if(typeof(comparator) == 'string'){		
-		var fieldtype = clause.find(".fields option:selected").attr('data-fieldtype');
-		for (var i in comparators[fieldtype]){
-			if (comparators[fieldtype][i].Comparator == comparator){
-				comparator = comparators[fieldtype][i];
+		ARGUMENT_COUNTS: {'string': 1}
+
+		defaults: function(){
+			return {
+				field: fields.at(0), 
+				comparator: 'is',
+				values: []
+			};
+		},
+
+		validate: function(attrs, options){
+			// validate value counts for comparator
+			if(attrs.comparator == 'between'){
+				if(attrs.values.length != 2){
+					return "Invalid number of values for " + attrs.comparator;
+				}
+			} else {
+				if(attrs.values.length != 1){
+					return "Invalid number of values for " + attrs.comparator;
+				}
+			}
+
+			// validate comparator works for field type
+			if(! _.contains(this.COMPARATORS[attrs.field.attributes.fieldType], attrs.comparator)){
+				return 'Invalid comparator for ' + attrs.field.attributes.fieldType;
+			}
+
+			// validate that the field is a valid field
+			if(! _.contains(fields, attrs.fields)) {
+				return "Invalid field";
 			}
 		}
-	}
-	
-	switch(comparator.ArgumentCount)
-	{
-		case 1:
-			// add textbox to .conditions <span>
-			$("<input/>")
-				.val(comparator.ArgumentDefaults[0])
-				.attr("type","text")
-				.appendTo(conditions);
-			break;
-		case 2:
-			// add a textbox, then a span, then another textbox 
-			$("<input/>")
-				.val(comparator.ArgumentDefaults[0])
-				.attr("type","text")
-				.appendTo(conditions);
-			$("<span/>")
-				.text("and")
-				.appendTo(conditions);
-			$("<input/>")
-				.val(comparator.ArgumentDefaults[0])
-				.attr("type","text")
-				.appendTo(conditions);
-			break;
-	}
-}
+
+		// TODO: check whether these things are valid before use
+		// http://www.sagarganatra.com/2012/10/backbonejs-model-validation-in.html
+	});
+
+	var FieldList = Backbone.Collection.extend({
+		model: Field
+	});
+	var ClauseList = Backbone.Collection.extend({
+		model: Clause,
+		// local storage to make this easily demoable
+		localStorage: new Backbone.LocalStorage("clauses-backbone")
+	});
+
+	var fields = new FieldList;
+	var clauses = new ClauseList;
+
+
+	var ClauseView = Backbone.View.extend({
+		tagName: 'li',
+
+		template: _.template($('#clause-template').html()),
+
+		events: {
+			"click a.destroy" : "clear",
+			"change .clause-field" : "changeField",
+			"change .clause-comparator" : "changeComparator",
+			"change input" : "changeValue"
+		},
+
+
+		initialize: function(){
+			this.listenTo(this.model, 'change', this.render);
+			this.listenTo(this.model, 'destroy', this.remove);
+		},
+
+		render: function(){
+			this.$el.html(this.template({
+				field: this.model.get('field').toJSON(),
+				comparator: this.model.get('comparator'),
+				values: this.model.get('values'),
+				fields: fields.toJSON(),
+				COMPARATORS: this.model.COMPARATORS
+			}));
+			return this;
+		},
+
+		clear: function(){
+			this.model.destroy();
+		}, 
+
+		changeField: function(){
+			// change the field object to the appropriate one
+			var new_field = fields.findWhere({fieldName: this.$el.children('.clause-field').val() });
+			this.model.set({'field': new_field});
+
+			// change the comparator
+			var new_comparator = _.first(this.model.COMPARATORS[new_field.get('fieldType')]);
+			this.model.set({'comparator': new_comparator});
+
+			this.render();
+		},
+
+		changeComparator: function(){
+			this.model.set({'comparator': this.$el.children('.clause-comparator').val()});
+			// when switching to a between comparator, dupe the value into the 2nd parameter
+			if(this.model.get('comparator') == 'between'){
+				var old_value = this.model.get('values')[0]; 
+				this.model.set({'values': [old_value, old_value]});
+			}
+			this.render();
+		},
+
+		changeValue: function(){
+			var new_vals = [];
+			this.$el.children('input').each(function(){
+				new_vals.push($(this).val());
+			});
+			this.model.set({'values': new_vals});
+		}
+	});
+
+	var AppView = Backbone.View.extend({
+		el: $("#filterbuilder"),
+
+		initialize: function(element){
+			// populate the Fields, Clauses collections from passed params
+			_.each(this.options.json_fields, function(element){
+				var f = new Field(element);
+				if(f.isValid()){
+					fields.add(f);
+				}
+			});
+			_.each(this.options.json_clauses, function(element){
+				var c = new Clause();
+				c.set({
+					field: fields.findWhere({ fieldName: element.fieldName }), 
+					comparator: element.comparator,
+					values: element.values 
+				});
+
+				if(c.isValid()){
+					clauses.add(c);				
+				}
+
+				var vc = new ClauseView({model: c});
+				this.$('#clauses').append(vc.render().el);
+			});
+		}, 
+
+		events: {
+			"click #clause-add": "addClause"
+		},
+
+		addClause: function(){
+			c = clauses.create(new Clause);
+			var vc = new ClauseView({model: c});
+			this.$('#clauses').append(vc.render().el);
+		}
+	});
+
+	var App = new AppView({json_fields:json_fields, json_clauses:json_clauses});
+});
